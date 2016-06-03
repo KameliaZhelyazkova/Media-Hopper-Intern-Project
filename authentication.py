7d2fa3451a1a8ccdeecfe4dccbafddc26fee0000
KameliaZhelyazkova <kamelia.zhelyazkova@gmail.com>
2016-06-03 12:30:15 +0100

    Authentication .py file added

--- authentication.py
+++ authentication.py
@@ -5,7 +5,7 @@ userID = "kamelia.zhelyazkova@ed.ac.uk"
 ks_type = 2
 admin_secret = "1a7227978d8228dde2a574fac2c9b371"
 partnerID = 1817881
-
+#
 config = KalturaConfiguration(partnerID)
 config.serviceUrl = "http://www.kaltura.com/"
 client = KalturaClient(config)
