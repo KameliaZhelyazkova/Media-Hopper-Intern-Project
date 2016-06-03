5376d3e782da40f0651dee2c7daa219bcf0f9e3d
crowec <connie81095@orange.fr>
2016-06-03 15:47:22 +0100

    Cleaned up file, recreated test so I don't break it.

---
+++ test.py
@@ -0,0 +1,39 @@
+from KalturaClient import *
+#from KalturaClient import Plugins
+
+userID = "kamelia.zhelyazkova@ed.ac.uk"
+ks_type = 2
+admin_secret = "1a7227978d8228dde2a574fac2c9b371"
+partnerID = 1817881
+#
+config = KalturaConfiguration(partnerID)
+config.serviceUrl = "http://www.kaltura.com/"
+client = KalturaClient(config)
+
+
+ks = client.generateSession(admin_secret, userID, ks_type, partnerID)
+client.setKs(ks)
+
+print "Retrieving..."
+entryId = "1_j5689kew";
+
+try:
+    mediaEntry = client.media.get(entryId)
+    print mediaEntry.getName()
+except Base.KalturaException, e:
+    print "could not get entry from Kaltura. Reason: %s" % repr(e)
+
+print "Done."
+
+#filter = Plugins.Core.KalturaMediaEntryFilter()
+#filter.freeText = "1_j5689kew"
+#filter.orderBy = "-weight"
+#filter.advancedSearch = Plugins.Metadata.KalturaMetadataSearchItem()
+
+#pager = Plugins.Core.KalturaFilterPager()
+#results = client.media.list(filter, pager)
+#print results
+
+#f = open('out.txt', 'w')
+#f.write(results)
+#f.close()
